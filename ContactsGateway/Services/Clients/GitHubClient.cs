37c228b1ddbdbae8b0a5722e60ae234558c2374c
Naoki Ikeguchi <root@siketyan.dev>
2020-07-24 23:50:36 +0900

    ✨ Add a simple client implementation for GitHub

---
+++ ContactsGateway/Services/Clients/GitHubClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ContactsGateway.Services.Clients
+{
+    public class GitHubClient
+    {
+        private const string BASE_URL = "https://api.github.com/";
+        
+        private readonly HttpClient _httpClient;
+        private readonly string _token;
+        
+        public GitHubClient(HttpClient httpClient, string token)
+        {
+            _httpClient = httpClient;
+            _token = token;
+        }
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var message = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(BASE_URL + url),
+                Headers =
+                {
+                    { "User-Agent", "Contacts-Gateway" },
+                    { "Authorization", $"Bearer {_token}" }
+                }
+            };
+            
+            return JsonConvert.DeserializeObject<T>(
+                await (await _httpClient.SendAsync(message)).Content.ReadAsStringAsync()
+            );
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
