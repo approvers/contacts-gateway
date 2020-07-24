9180bcea4082c79aa2fffd9e72a926b8d8350c96
Naoki Ikeguchi <root@siketyan.dev>
2020-07-24 23:54:38 +0900

    ✨ Configure services

--- ContactsGateway/Startup.cs
+++ ContactsGateway/Startup.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using ContactsGateway.Models.Contacts;
+using ContactsGateway.Services.Clients;
+using ContactsGateway.Services.Fetchers;
+using CoreTweet;
+using Discord;
+using Discord.Rest;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -15,9 +22,15 @@ namespace ContactsGateway
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostEnvironment environment)
         {
-            Configuration = configuration;
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(environment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.local.json", true)
+                .AddJsonFile($"appsettings.{environment.EnvironmentName}.json", true)
+                .AddEnvironmentVariables()
+                .Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -26,6 +39,31 @@ namespace ContactsGateway
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddScoped(provider => OAuth2.GetToken(
+                Configuration["Twitter:ConsumerKey"],
+                Configuration["Twitter:ConsumerSecret"]
+            ));
+
+            services.AddScoped(provider => new GitHubClient(
+                new HttpClient(),
+                Configuration["GitHub:Token"]
+            ));
+
+            services.AddScoped(provider =>
+            {
+                var client = new DiscordRestClient();
+
+                client
+                    .LoginAsync(TokenType.Bot, Configuration["Discord:Token"])
+                    .Wait();
+
+                return client;
+            });
+
+            services.AddScoped<TwitterFetcher>();
+            services.AddScoped<GitHubFetcher>();
+            services.AddScoped<DiscordFetcher>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
