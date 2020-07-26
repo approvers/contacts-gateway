91cc34ca31092a2cad3a773b1ae303e07a1d7911
Naoki Ikeguchi <root@siketyan.dev>
2020-07-26 23:51:45 +0900

    ✨ Add cache and cached implementations

---
+++ ContactsGateway/Services/Caching/Cache/TimeoutCache.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using ContactsGateway.Services.Caching.Storage;
+
+namespace ContactsGateway.Services.Caching.Cache
+{
+    public class TimeoutCache<T> : ICache<T> where T : ICacheable
+    {
+        public class Entry : ICacheEntry<T>
+        {
+            public T Item { get; }
+            public DateTimeOffset CachedAt { get; }
+
+            public Entry(T item, DateTimeOffset cachedAt)
+            {
+                Item = item;
+                CachedAt = cachedAt;
+            }
+        }
+
+        public class EntryFactory : ICacheEntryFactory<Entry, T>
+        {
+            public Entry Create(T item)
+            {
+                return new Entry(
+                    item,
+                    DateTimeOffset.Now
+                );
+            }
+        }
+        
+        private readonly ICacheStorage<Entry, T> _storage;
+        private readonly EntryFactory _entryFactory;
+        private readonly TimeSpan _timeout;
+        
+        public TimeoutCache(ICacheStorage<Entry, T> storage, EntryFactory entryFactory, TimeSpan timeout)
+        {
+            _storage = storage;
+            _entryFactory = entryFactory;
+            _timeout = timeout;
+        }
+
+        public async Task<T> HitOrUpdateAsync(ulong id, Func<ulong, Task<T>> updater)
+        {
+            if (await _storage.HasAsync(id))
+            {
+                var entry = await _storage.RestoreAsync(id);
+                if (entry.CachedAt + _timeout > DateTimeOffset.Now)
+                {
+                    return entry.Item;
+                }
+            }
+
+            var item = await updater(id);
+            await _storage.StoreAsync(
+                _entryFactory.Create(item)
+            );
+
+            return item;
+        }
+    }
+}
---
+++ ContactsGateway/Services/Fetchers/CachedFetcher.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using ContactsGateway.Services.Caching;
+using ContactsGateway.Services.Caching.Cache;
+
+namespace ContactsGateway.Services.Fetchers
+{
+    public class CachedFetcher<T> : IFetcher<T> where T : ICacheable
+    {
+        private readonly ICache<T> _cache;
+        private readonly IFetcher<T> _fetcher;
+        
+        public CachedFetcher(ICache<T> cache, IFetcher<T> fetcher)
+        {
+            _cache = cache;
+            _fetcher = fetcher;
+        }
+
+        public Task<T> FetchAsync(ulong id)
+        {
+            return _cache.HitOrUpdateAsync(id, _fetcher.FetchAsync);
+        }
+    }
+}
