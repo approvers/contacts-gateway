a01995602fb063de58225c93da99f929e6a3d715
Naoki Ikeguchi <root@siketyan.dev>
2020-07-26 23:56:38 +0900

    🔧 Configure dependency injection

--- ContactsGateway/Startup.cs
+++ ContactsGateway/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using ContactsGateway.Models.Contacts;
+using ContactsGateway.Services.Caching.Cache;
+using ContactsGateway.Services.Caching.Storage;
 using ContactsGateway.Services.Clients;
 using ContactsGateway.Services.Fetchers;
 using CoreTweet;
@@ -63,8 +65,49 @@ namespace ContactsGateway
             });
 
             services.AddScoped<TwitterFetcher>();
-            services.AddScoped<GitHubFetcher>();
             services.AddScoped<DiscordFetcher>();
+            services.AddScoped<GitHubFetcher>();
+
+            services.AddTransient<TimeoutCache<TwitterContact>.EntryFactory>();
+            services.AddTransient<TimeoutCache<DiscordContact>.EntryFactory>();
+            services.AddTransient<TimeoutCache<GitHubContact>.EntryFactory>();
+
+            services.AddSingleton<ICacheStorage<TimeoutCache<TwitterContact>.Entry, TwitterContact>, InMemoryCacheStorage<TimeoutCache<TwitterContact>.Entry, TwitterContact>>();
+            services.AddSingleton<ICacheStorage<TimeoutCache<GitHubContact>.Entry, GitHubContact>, InMemoryCacheStorage<TimeoutCache<GitHubContact>.Entry, GitHubContact>>();
+            services.AddSingleton<ICacheStorage<TimeoutCache<DiscordContact>.Entry, DiscordContact>, InMemoryCacheStorage<TimeoutCache<DiscordContact>.Entry, DiscordContact>>();
+
+            services.AddSingleton<ICache<TwitterContact>, TimeoutCache<TwitterContact>>(provider => new TimeoutCache<TwitterContact>(
+                provider.GetService<ICacheStorage<TimeoutCache<TwitterContact>.Entry, TwitterContact>>(),
+                provider.GetService<TimeoutCache<TwitterContact>.EntryFactory>(),
+                TimeSpan.FromMilliseconds(int.Parse(Configuration["Cache:Timeout"])))
+            );
+            
+            services.AddSingleton<ICache<GitHubContact>, TimeoutCache<GitHubContact>>(provider => new TimeoutCache<GitHubContact>(
+                provider.GetService<ICacheStorage<TimeoutCache<GitHubContact>.Entry, GitHubContact>>(),
+                provider.GetService<TimeoutCache<GitHubContact>.EntryFactory>(),
+                TimeSpan.FromMilliseconds(int.Parse(Configuration["Cache:Timeout"])))
+            );
+            
+            services.AddSingleton<ICache<DiscordContact>, TimeoutCache<DiscordContact>>(provider => new TimeoutCache<DiscordContact>(
+                provider.GetService<ICacheStorage<TimeoutCache<DiscordContact>.Entry, DiscordContact>>(),
+                provider.GetService<TimeoutCache<DiscordContact>.EntryFactory>(),
+                TimeSpan.FromMilliseconds(int.Parse(Configuration["Cache:Timeout"])))
+            );
+
+            services.AddScoped<IFetcher<TwitterContact>, CachedFetcher<TwitterContact>>(provider => new CachedFetcher<TwitterContact>(
+                provider.GetService<ICache<TwitterContact>>(),
+                provider.GetService<TwitterFetcher>()
+            ));
+            
+            services.AddScoped<IFetcher<GitHubContact>, CachedFetcher<GitHubContact>>(provider => new CachedFetcher<GitHubContact>(
+                provider.GetService<ICache<GitHubContact>>(),
+                provider.GetService<GitHubFetcher>()
+            ));
+            
+            services.AddScoped<IFetcher<DiscordContact>, CachedFetcher<DiscordContact>>(provider => new CachedFetcher<DiscordContact>(
+                provider.GetService<ICache<DiscordContact>>(),
+                provider.GetService<DiscordFetcher>()
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
